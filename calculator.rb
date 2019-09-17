ac64199ead18d501d7e819d8ca92269b62c16b52
Jordan Long <jordanleelong5@gmail.com>
2019-09-17 01:41:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 first_number = 2.0
 second_number = 3.0
 sum = first_number + second_number
-difference = first_number - second_number 
+difference = first_number - second_number
+product = first_number * second_number 
